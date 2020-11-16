a076116c7a3455e38069ae6e86f98777786ea125
gay0ung <6and_end9@naver.com>
2020-11-16 21:57:12 +0900

    section6 - aliases

---
+++ TypeScript/class-note/4_type-aliases.ts
@@ -0,0 +1,23 @@
+// 인터페이스인 경우
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// 타입별칭인 경우
+type Person = {
+  name: string;
+  age: number;
+}
+
+let young: Person = {
+  name: 'young e',
+  age: 26
+}
+
+type MyString = string;
+let str: MyString = 'hello'
+
+type Todo = {id: string; title: string; done: boolean};
+function getTodo(todo: Todo){}
+
