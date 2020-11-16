59a5e7a848c85fdb3cf6697540942557464c670f
gay0ung <6and_end9@naver.com>
2020-11-16 18:06:23 +0900

    section03

--- TypeScript/learn-typescript/quiz/1_todo/.eslintrc.js
+++ TypeScript/learn-typescript/quiz/1_todo/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         semi: true,
         useTabs: false,
         tabWidth: 2,
+        trailingComma: 'all',
         printWidth: 80,
         bracketSpacing: true,
         arrowParens: 'avoid',
--- TypeScript/learn-typescript/quiz/1_todo/src/index.ts
+++ TypeScript/learn-typescript/quiz/1_todo/src/index.ts
@@ -1,7 +1,7 @@
-let todoItems;
+let todoItems: object[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,20 +11,21 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: object): void {
   todoItems.push(todo);
+  // 반환값이 없을경우에느 void를 사용해준다.
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: object): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -41,6 +42,8 @@ function showCompleted() {
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems() {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo(todo);
+  addTodo(todo);
 }
 
 // NOTE: 유틸 함수
