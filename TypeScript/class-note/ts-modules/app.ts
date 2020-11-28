39cf3c60fd2b3db79a4ee5202135e60740ac2dfa
gay0ung <6and_end9@naver.com>
2020-11-28 13:52:14 +0900

    16 타입 모듈화

---
+++ TypeScript/class-note/ts-modules/app.ts
@@ -0,0 +1,6 @@
+import { Todo } from './types'
+
+let item: Todo = {
+  title: '할일 1',
+  checked: false,
+}
--- TypeScript/learn-typescript/quiz/2_address-book/src/index.ts
+++ TypeScript/learn-typescript/quiz/2_address-book/src/index.ts
@@ -1,21 +1,4 @@
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneType {
-  Home = 'home',
-  Office = 'office',
-  Studio = 'studio',
-}
-
+import { Contact, PhoneType } from './types';
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
 function fetchContacts(): Promise<Contact[]> {
