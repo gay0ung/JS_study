412b8fbc7c3e2cbb507f7a1f8748f2b146348b7b
gay0ung <58800923+gay0ung@users.noreply.github.com>
2022-11-22 20:37:33 +0900

    chapter1-3:Logical NULLISH Assignment

--- ES2021&ES2022/index.js
+++ ES2021&ES2022/index.js
@@ -1,25 +1,36 @@
 // Logical Assignment Operators
 // 1. Logical OR Assignment
-let name = prompt("what is your name?");
+// let name = prompt("what is your name?");
 
-// if(!name) {
-//     name = "anonymous";
-// }
+// // if(!name) {
+// //     name = "anonymous";
+// // }
 
-name ||= "anonymous";
+// name ||= "anonymous";
 
-console.log(`Hello ${name}`);
+// console.log(`Hello ${name}`);
 
 // 2. Logical AND Assignment
-const user = {
-    username: 'young',
-    password: 1234
-}
-
-// if (user.password) {
-//     user.password = "[secret]"
+// const user = {
+//     username: 'young',
+//     password: 1234
 // }
 
-user.password &&= "[secret]";
+// // if (user.password) {
+// //     user.password = "[secret]"
+// // }
+
+// user.password &&= "[secret]";
+
+// console.log(user);
+
+// 3. Logical NULLISH Assignment
+const user = {
+    username: 'young',
+    password: 1234,
+    isAdmin: undefined
+}
+
+user.isAdmin ??= true; 
 
 console.log(user);
